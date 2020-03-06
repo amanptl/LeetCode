81d426af077400466c5f425e4a75fb983462fa08
Aman Patel <amanptl@users.noreply.github.com>
2020-03-06 16:55:01 -0500

    By difficulty

---
+++ Easy/Maximum Level Sum of a Binary Tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxLevelSum(self, root: TreeNode) -> int:
+        a = {}
+        def level(node,depth):
+            if node:
+                if depth in a:
+                    a[depth]+=node.val
+                else:
+                    a[depth] = node.val
+                if node.left:
+                    level(node.left, depth+1)
+                if node.right:
+                    level(node.right, depth+1)
+                
+        
+        level(root,1)
+        m = 0
+        for i in a:
+            if a[i] > m:
+                m = i
+        return m
